f2ca648c37650f6e6231cf518c8e9a48205796d9
アリフ　マウラナ <a_maulana@sbs-infosys.co.jp>
2021-09-03 15:59:20 +0900

    bug fix

--- app/controllers/landings_controller.rb
+++ app/controllers/landings_controller.rb
@@ -19,17 +19,6 @@ class LandingsController < ApplicationController
     @googleapikey = ENV['GOOGLE_API_KEY']
     sprs = SalesProductRelation.where("sales_point_id=?",@sales_point.id)
     @product_choices = {}
-    current_user.products.each do |product|
-      exist = false
-      sprs.each do |spr|
-        if product.id == spr.product_id
-          exist = true
-        end
-      end
-      if exist == false 
-        @product_choices[product.name] = product.id
-      end
-    end
     render 'sales_points/show'
   end
 
